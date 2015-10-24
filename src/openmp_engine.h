971337f45d2897c70de5e0be14809edeb1b33f45
Yuri Blokhin <ultrablox@gmail.com>
2015-10-24 10:38:38 +0300

    Bugfix

--- src/openmp_engine.h
+++ src/openmp_engine.h
@@ -23,20 +23,22 @@ public:
 
 		std::atomic<bool> found(false);
 #pragma omp parallel
-		cout << "Starting OPENMP thread " << omp_get_thread_num() << " (total " << omp_get_num_threads() << " threads)" << std::endl;
-		do
 		{
-			typename bf_t::key_t found_key;
-
-			auto cur_chunk = gen.next_chunk();
-			bool r = (*this->pBF)(cur_chunk.key_length, cur_chunk.first, cur_chunk.last, found_key);
-			cout << "Thread " << omp_get_thread_num() << ": processed " << cur_chunk << " (res=" << r << ")" << std::endl;
-			if(r)
+			cout << "Starting OPENMP thread " << omp_get_thread_num() << " (total " << omp_get_num_threads() << " threads)" << std::endl;
+			do
 			{
-				found.store(true);
-				this->correctKey = found_key;
-			}
-		} while(!found.load());
+				typename bf_t::key_t found_key;
+
+				auto cur_chunk = gen.next_chunk();
+				bool r = (*this->pBF)(cur_chunk.key_length, cur_chunk.first, cur_chunk.last, found_key);
+				cout << "Thread " << omp_get_thread_num() << ": processed " << cur_chunk << " (res=" << r << ")" << std::endl;
+				if(r)
+				{
+					found.store(true);
+					this->correctKey = found_key;
+				}
+			} while(!found.load());
+		}
 	}
 };
 
